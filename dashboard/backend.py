5c5deaab54fad58542c8921219196f51a4117670
Shuhao Zhang <jeremyz0903@gmail.com>
2023-01-31 15:55:28 +0800

    edit date

--- dashboard/backend.py
+++ dashboard/backend.py
@@ -37,3 +37,16 @@ class VehicleInfo:
         -Hatchback: Compact sedan, capacity: 4 <br/>\
         -Minivan: Trunks with large cargo area, capacity: 7 <br/>\
         * The capacity includes driver. "
+
+class DateTimePicker(DateTimeInput):
+    template_name = 'dashboard/DateTimePicker.html'
+
+    def get_context(self, name, value, attrs):
+        datetimepicker_id = 'datetimepicker_{name}'.format(name=name)
+        if attrs is None:
+            attrs = dict()
+        attrs['data-target'] = '#{id}'.format(id=datetimepicker_id)
+        attrs['class'] = 'form-control datetimepicker-input'
+        context = super().get_context(name, value, attrs)
+        context['widget']['datetimepicker_id'] = datetimepicker_id
+        return context
--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Group, Vehicle, Ride
 from django.core.exceptions import ValidationError
+from  .backend import DateTimePicker
 
 # validation functions
 def validate_positive(value):
@@ -37,10 +38,24 @@ class RideRequestForm(forms.ModelForm):
             attrs={'cols': '10', 'rows': '1',
                    'placeholder': 'Enter your address here.'}),
         min_length=0,
-        max_length=100
+        max_length=100,
     )
 
-    passengerNum = forms.IntegerField(max_value=10, min_value=1)
+    arrivalTime = forms.DateTimeField(
+        label="Desired Arrival Time",
+        input_formats=['%d/%m/%Y %H:%M'],
+        widget=DateTimePicker(
+            attrs={
+                'placeholder': "dd/mm/yyyy hh:mm",
+            }
+        )
+    )
+    passengerNum = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={'cols': '5', 'rows': '1',
+                   'placeholder': 'Enter number here.'}),
+        max_value=10, min_value=1
+    )
 
     class Meta:
         model = Ride
@@ -62,37 +77,3 @@ class RideRequestForm(forms.ModelForm):
     #             % (vtinfo.type_choices[vehicleType][1])
     #         )
     #     return cleaned_data
-# class UserCreationForm(forms.ModelForm):
-#     """
-#     A form that creates a user, with no privileges, from the given username and
-#     password.
-#     """
-#     error_messages = {
-#         'password_mismatch': _("The two password fields didn't match."),
-#     }
-#     password1 = forms.CharField(label=_("Password"),
-#         widget=forms.PasswordInput)
-#     password2 = forms.CharField(label=_("Password confirmation"),
-#         widget=forms.PasswordInput,
-#         help_text=_("Enter the same password as above, for verification."))
-#
-#     class Meta:
-#         model = User
-#         fields = ("username",)
-#
-#     def clean_password2(self):
-#         password1 = self.cleaned_data.get("password1")
-#         password2 = self.cleaned_data.get("password2")
-#         if password1 and password2 and password1 != password2:
-#             raise forms.ValidationError(
-#                 self.error_messages['password_mismatch'],
-#                 code='password_mismatch',
-#             )
-#         return password2
-#
-#     def save(self, commit=True):
-#         user = super(UserCreationForm, self).save(commit=False)
-#         user.set_password(self.cleaned_data["password1"])
-#         if commit:
-#             user.save()
-#         return user
