c713403a98f193f7cbe4e2a719120066073c9183
Shuhao Zhang <jeremyz0903@gmail.com>
2023-02-03 14:14:38 +0800

    fix password

--- uber/settings.py
+++ uber/settings.py
@@ -156,4 +156,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jeremyz0903@gmail.com'
-EMAIL_HOST_PASSWORD = 'Zsh13964989099'
+EMAIL_HOST_PASSWORD = 'rxqbwsrudwyycvqn'
